a75ff9536705c15ba1cf4321554dc412e8b1ea1b
Saullo Santos <saullo.s.r@hotmail.com>
2024-06-25 21:03:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id" : "1",
+        "photo": "https://allianzparque.com.br/wp-content/uploads/2024/04/site-bring-me-the-horizon.jpg",
+        "title": "Evento do BMTH anunciado 🤘",
+        "description": "Show da banda acontece em São Paulo no fim do mes de novembro e conta com outras bandas conhecidas como Motionless in Wihte e Spiritbox "
+    },
+    {
+        "id" : "2",
+        "photo": "aaa",
+        "title": "Saui nova musica",
+        "description": "oioi"
+    }
+]
